73382bb0d7f1ea2fe437f8397e4d78c2b6a9354e
Big Papa <bigpapa@Bigs-MacBook-Pro.local>
2022-01-06 20:28:09 -0600

    Working on adding more pages, having issues w switch the columns.

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,54 @@
+export const homeObjOne = 
+{
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Top Line',
+    headLine: 'HeadLine',
+    description: 'description',
+    buttonLabel: 'Button Label',
+    imgStart: true,
+    imag: require('../../images/svg-2.svg'), 
+    // ^SVGs can be pulled from Udraw, tryna see if I can finesse this with pngs
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+// export const homeObjTwo = 
+// {
+//     id: 'discover',
+//     lightBg: true,
+//     lightText: false,
+//     lightTextDesc: false,
+//     topLine: 'Top Line 2',
+//     headLine: 'HeadLine 2',
+//     description: 'description 2',
+//     buttonLabel: 'Button Label 2',
+//     imgStart: true,
+//     img: require('../../images/svg-2.png'), 
+//     // ^SVGs can be pulled from Udraw, tryna see if I can finesse this with pngs
+//     alt: 'Piggybank',
+//     dark: true,
+//     primary: true,
+//     darkText: false
+// };
+// export const homeObjThree = 
+// {
+//     id: 'about',
+//     lightBg: false,
+//     lightText: true,
+//     lightTextDesc: true,
+//     topLine: 'Top Line',
+//     headLine: 'HeadLine',
+//     description: 'description',
+//     buttonLabel: 'Button Label',
+//     imgStart: false,
+//     img: require('../../images/svg-3.png'), 
+//     // ^SVGs can be pulled from Udraw, tryna see if I can finesse this with pngs
+//     alt: 'Car',
+//     dark: true,
+//     primary: true,
+//     darkText: false
+// }
